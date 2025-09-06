7dde29ac658f18f9da8d17f3b69dae9ae550873e
RubenCano21 <ruben217.rc@gmail.com>
2025-09-06 00:10:05 -0400

    first commit

---
+++ src/main/java/org/example/schemaflow/SchemaFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.schemaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaFlowApplication.class, args);
+	}
+
+}
